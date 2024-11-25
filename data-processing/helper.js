73de3b4ddae185e7d8546b42ed2e0164c5b9399a
ienming <hsieh.alan.0726@gmail.com>
2024-11-25 21:27:16 +0800

    feat: compress photo and save to JSON

--- data-processing/helper.js
+++ data-processing/helper.js
@@ -1,6 +1,8 @@
 import exifParser from 'exif-parser';
 import { writeFile } from 'node:fs';
 import ColorThief from 'colorthief';
+import sharp from 'sharp';
+import fs from 'fs/promises';
 
 export function getGpsCoordinates(buffer) {
     const parser = exifParser.create(buffer);
@@ -16,22 +18,33 @@ export function getGpsCoordinates(buffer) {
     }
 }
 
-export function writeData() {
-    writeFile('message.txt', 'Hello Node.js', 'utf8', () => {
-        console.log('success');
-    });
+export async function getColor(path) {
+    try {
+        const color = await ColorThief.getColor(path);
+        console.log(`取得照片顏色: ${path}`);
+        return color;
+    } catch (error) {
+        throw new Error(`取得照片顏色失敗: ${error.message}`);
+    }
 }
 
-export function getColor(path) {
-    return new Promise((resolve, reject) => {
-        ColorThief.getColor(path)
-            .then(color => {
-                console.log(`取得照片顏色: ${path}`);
-                resolve(color);
-            })
-            .catch(err => {
-                console.error(err);
-                reject(err);
-            });
-    });
+export async function compressPhoto(inputPath, outputDir) {
+    try {
+        await fs.mkdir(outputDir, { recursive: true });
+
+        const data = await sharp(inputPath)
+            .jpeg({
+                mozjpeg: true,
+                quality: 50,
+             })
+            .toBuffer();
+
+        const imgName = inputPath.split('/').pop();
+        const outputPath = `${outputDir}/${imgName}`;
+        await fs.writeFile(outputPath, data);
+
+        console.log('圖片壓縮並儲存完成！');
+    } catch (error) {
+        throw new Error(`處理圖片時發生錯誤: ${error.message}`)
+    }
 }
--- data-processing/readPhoto.js
+++ data-processing/readPhoto.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { getColor, getGpsCoordinates } from './helper.js';
+import { getColor, getGpsCoordinates, compressPhoto } from './helper.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,23 +13,39 @@ const allImgs = await Promise.all(
     fs.readdirSync(folderPath).map(async fileName => {
         const result = availableFormat.some(format => fileName.includes(format));
         if (result) {
-            const imgPath = path.join(folderPath, fileName);
-            const buffer = fs.readFileSync(imgPath);
-
-            // 取得 GPS 座標和顏色
-            const coordinates = getGpsCoordinates(buffer);
-            const color = await getColor(imgPath);
-
-            if (coordinates) {
-                coordinates.name = fileName;
-                coordinates.color = color;
-                return coordinates;
+            try {
+                const imgPath = path.join(folderPath, fileName);
+                const buffer = fs.readFileSync(imgPath);
+    
+                // 取得 GPS 座標和顏色
+                const coordinates = getGpsCoordinates(buffer);
+                const color = await getColor(imgPath);
+    
+                // 壓縮照片
+                const compressedFolderPath = path.join(__dirname, '../compressed');
+                await compressPhoto(imgPath, compressedFolderPath);
+    
+                if (coordinates) {
+                    coordinates.name = fileName;
+                    coordinates.color = color;
+                    console.log(coordinates);
+                    return coordinates;
+                }
+            } catch (error) {
+                return null;
             }
+        } else {
+            return null;
         }
-        return null;
     })
 );
 
-// 過濾掉空值
-const filteredImgs = allImgs.filter(item => item !== null);
-console.log(filteredImgs);
+const validImgs = allImgs.filter(img => img);
+const outputDataPath = path.join(__dirname, '../frontend/public/imagesData.json');
+fs.writeFile(outputDataPath, JSON.stringify(validImgs, null, 2), 'utf8', (err) => {
+    if (err) {
+        console.error('寫入 JSON 檔案失敗:', err);
+    } else {
+        console.log('JSON 檔案寫入成功！');
+    }
+});
